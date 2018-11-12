ed11393bc572a152f05bc907468131ab8737a8b0
سليمان السهمي  (Soolaïman Sahmi) <sahmi.soulaimane@gmail.com>
2018-11-12 23:13:02 +0000

    Extend genmovreg() to support exchange between SSE and general purpose registers

--- src/dmd/backend/code.d
+++ src/dmd/backend/code.d
@@ -522,6 +522,7 @@ void genpop(ref CodeBuilder cdb, reg_t reg);
 void gensavereg(ref CodeBuilder cdb, ref reg_t reg, targ_uns slot);
 code *genmovreg(uint to, uint from);
 void genmovreg(ref CodeBuilder cdb, uint to, uint from);
+void genmovreg(ref CodeBuilder cdb, uint to, uint from, tym_t tym);
 void genmulimm(ref CodeBuilder cdb,uint r1,uint r2,targ_int imm);
 void genshift(ref CodeBuilder cdb);
 void movregconst(ref CodeBuilder cdb,reg_t reg,targ_size_t value,regm_t flags);
