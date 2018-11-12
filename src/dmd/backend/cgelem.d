0ad0362e72d6732b4770498b6ef4efa50ca2ea29
سليمان السهمي  (Soolaïman Sahmi) <sahmi.soulaimane@gmail.com>
2018-11-12 23:55:34 +0000

    struct containing a SIMD 32 bytes vector

--- src/dmd/backend/cgelem.d
+++ src/dmd/backend/cgelem.d
@@ -3315,6 +3315,13 @@ elem * elstruct(elem *e, goal_t goal)
                 goto L1;
             goto Ldefault;
 
+        case 32:
+            if (ty == TYstruct && I64 && config.exe != EX_WIN64)
+            {
+                goto L1;
+            }
+            goto Ldefault;
+
         L1:
             if (ty == TYstruct)
             {   // This needs to match what TypeFunction::retStyle() does
