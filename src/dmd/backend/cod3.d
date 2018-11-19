6e7c885d664033c36fb785bd3a562f66a406da84
سليمان السهمي  (Soolaïman Sahmi) <sahmi.soulaimane@gmail.com>
2018-11-19 00:22:57 +0000

    Special handling for static arrays small enough to go into registers. A "naked" static array and one that is wrapped in a struct should behave similarly. The spec says nothing about this but the comments in argtypes.d suggest this. Also people pass static arrays by value in c and c++ by wrapping a struct arround them.

--- src/dmd/backend/cod3.d
+++ src/dmd/backend/cod3.d
@@ -1340,7 +1340,6 @@ regm_t allocretregs(tym_t ty, type *t, tym_t tyf, reg_t *reg1, reg_t *reg2)
         ty1 = t.Tty;
     }
 
-Lagain:
     switch (tyrelax(ty1))
     {
         case TYcent:
@@ -1381,10 +1380,15 @@ Lagain:
             break;
 
         case TYarray:
-            ty1 = argtypeof(ty1, t);
-            if (ty1 == TYarray)
+            type* targ1, targ2;
+            argtypes(t, &targ1, &targ2);
+            if (targ1)
+                ty1 = targ1.Tty;
+            else
                 return 0;
-            goto Lagain;
+            if (targ2)
+                ty2 = targ2.Tty;
+            break;
 
         case TYstruct:
             assert(t);
@@ -4007,12 +4011,22 @@ void prolog_loadparams(ref CodeBuilder cdb, tym_t tyf, bool pushalloc, regm_t* n
 
             type *t = s.Stype;
             type *t2 = null;
-            if (tybasic(t.Tty) == TYstruct && config.exe != EX_WIN64)
+            if (tyaggregate(t.Tty) && config.exe != EX_WIN64)
             {
-                type *targ1 = t.Ttag.Sstruct.Sarg1type;
-                t2 = t.Ttag.Sstruct.Sarg2type;
-                if (targ1)
-                    t = targ1;
+                if (tybasic(t.Tty) == TYstruct)
+                {
+                    type *targ1 = t.Ttag.Sstruct.Sarg1type;
+                    t2 = t.Ttag.Sstruct.Sarg2type;
+                    if (targ1)
+                        t = targ1;
+                }
+                else if (tybasic(t.Tty) == TYarray)
+                {
+                    type *targ1;
+                    argtypes(t, &targ1, &t2);
+                    if (targ1)
+                        t = targ1;
+                }
             }
 
             if (Symbol_Sisdead(s, anyiasm))
