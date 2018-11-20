9e3c455edd330fe34ebfc0e6ea8afeeaa4bc1a22
سليمان السهمي  (Soolaïman Sahmi) <sahmi.soulaimane@gmail.com>
2018-11-20 18:38:29 +0000

    preserve type information when optimizing This caused cases with the -O flag in the test suite to not compile

--- src/dmd/backend/cgelem.d
+++ src/dmd/backend/cgelem.d
@@ -2181,6 +2181,7 @@ L2:
   /* c,e => e   */
   if (OTleaf(e1op) && !OTsideff(e1op) && !(e1.Ety & mTYvolatile))
   {     e2.Ety = e.Ety;
+        e2.ET = e.ET;
         e = el_selecte2(e);
         goto Lret;
   }
@@ -2226,6 +2227,7 @@ L2:
                 )
         {   // ((a = b),(a < 0)) => ((a = b) < 0)
             e1.Ety = e2.EV.E1.Ety;
+            e1.ET = e2.EV.E1.ET;
             e.EV.E1 = e2.EV.E1;
             e2.EV.E1 = e1;
             goto L1;
@@ -2238,6 +2240,7 @@ L2:
         {
             /* ((a = b),(a || c)) => ((a = b) || c)     */
             e1.Ety = e2.EV.E1.Ety;
+            e1.ET = e2.EV.E1.ET;
             e.EV.E1 = e2.EV.E1;
             e2.EV.E1 = e1;
             e = el_selecte2(e);
@@ -2256,6 +2259,7 @@ L2:
             {
                 e.EV.E1 = e2.EV.E2;
                 e1.Ety = e2.EV.E2.Ety;
+                e1.ET = e2.EV.E2.ET;
                 e2.EV.E2 = e1;
                 goto L1;
             }
@@ -3369,6 +3373,7 @@ elem * elstruct(elem *e, goal_t goal)
             // Convert (x streq (a?y:z)) to (x streq *(a ? &y : &z))
             if (e2.Eoper == OPcond)
             {   tym_t ty2 = e2.Ety;
+                type *t2  = e2.ET;
 
                 /* We should do the analysis to see if we can use
                    something simpler than TYfptr.
@@ -3377,6 +3382,7 @@ elem * elstruct(elem *e, goal_t goal)
                 e2 = el_una(OPaddr,typ,e2);
                 e2 = optelem(e2,GOALvalue);          /* distribute & to x and y leaves */
                 *pe2 = el_una(OPind,ty2,e2);
+                (*pe2).ET = t2;
                 break;
             }
             break;
@@ -5164,6 +5170,8 @@ beg:
                         goto retnull;
                     if (!goal)
                         e.Ety = e.EV.E2.Ety;
+                    else
+                        e.EV.E2.ET = e.ET;
                     e = el_selecte2(e);
                     return e;
                 }
@@ -5172,7 +5180,10 @@ beg:
                     return el_selecte1(e);
                 }
                 if (!goal)
+                {
                     e.Ety = e2.Ety;
+                    e.ET = e2.ET;
+                }
                 return e;
             }
 
