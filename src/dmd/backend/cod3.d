f645935f2e74b97e04950da1ed1d15baaa1ab143
سليمان السهمي  (Soolaïman Sahmi) <sahmi.soulaimane@gmail.com>
2018-12-08 01:29:04 +0000

    Bug Fix - SIMD 32 bytes argument sliced to 16 bytes when dumped to the stack

--- src/dmd/backend/cod3.d
+++ src/dmd/backend/cod3.d
@@ -4065,6 +4065,7 @@ void prolog_loadparams(ref CodeBuilder cdb, tym_t tyf, bool pushalloc, regm_t* n
                             cdb.genc1(op,modregxrm(2,preg,BPRM),FLconst,offset);
                             if (XMM0 <= preg && preg <= XMM15)
                             {
+                                checkSetVex(cdb.last(), t.Tty);
                             }
                             else
                             {
@@ -4083,6 +4084,7 @@ void prolog_loadparams(ref CodeBuilder cdb, tym_t tyf, bool pushalloc, regm_t* n
                                        modregxrm(2,preg,4),FLconst,offset);
                             if (preg >= XMM0 && preg <= XMM15)
                             {
+                                checkSetVex(cdb.last(), t.Tty);
                             }
                             else
                             {
