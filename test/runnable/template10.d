3f4f23967e1a551b2be05a7ae6b3432bf11d438a
سليمان السهمي  (Suleyman Sahmi) <sahmi.soulaimane@gmail.com>
2019-03-19 17:20:16 +0000

    extra test cases

--- test/runnable/template10.d
+++ test/runnable/template10.d
@@ -260,6 +260,189 @@ void test6()
 
 /********************************************/
 
+class C7
+{
+    int m = 10;
+    template A(alias a)
+    {
+        template B(alias b)
+        {
+            template C(alias c)
+            {
+                auto sum()
+                {
+                    return a + b + c + m;
+                }
+                class K
+                {
+                    int n;
+                    this(int i) { n = i; }
+                    auto sum()
+                    {
+                        return a + b + c + m + n;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void test7()
+{
+    if (__ctfe)
+        return; // nested classes not yet ctfeable
+
+    auto a = 1;
+    auto b = 2;
+    auto c = 3;
+
+    assert(new C7().A!a.B!b.C!c.sum() == 1+2+3+10);
+    assert(new C7().new C7.A!a.B!b.C!c.K(20).sum() == 1+2+3+10+20);
+}
+
+/********************************************/
+
+interface I8
+{
+    int get();
+    int sub(alias v)()
+    {
+        return get() - v;
+    }
+}
+
+class A8
+{
+    int m;
+    this(int m)
+    {
+        this.m = m;
+    }
+    this() {}
+}
+
+class B8 : A8, I8
+{
+    this(alias i)()
+    {
+        super(i);
+    }
+    auto add(alias v)()
+    {
+        return super.m + v;
+    }
+    int get()
+    {
+        return m;
+    }
+    this() {}
+}
+
+void test8()
+{
+    int a = 4;
+    int b = 2;
+    auto o = new B8;
+    o.__ctor!a;
+    assert(o.m == 4);
+    assert(o.add!b == 4+2);
+    assert(o.sub!b == 4-2);
+}
+
+/********************************************/
+
+struct A9
+{
+    int m;
+    auto add(alias a)()
+    {
+        m += a;
+    }
+}
+
+struct B9
+{
+    int i;
+    A9 a;
+    alias a this;
+}
+
+struct S9
+{
+    int j;
+    B9 b;
+    alias b this;
+}
+
+void test9()
+{
+    int a = 9;
+    auto o = S9(1, B9(1, A9(10)));
+    o.add!9();
+    assert(o.b.a.m == 10+9);
+}
+
+/********************************************/
+
+struct S10
+{
+    int m;
+    auto add(alias f)(int a)
+    {
+        auto add(int b) { return a + b; }
+        return exec2!(f, add)();
+    }
+    auto exec2(alias f, alias g)()
+    {
+        return g(f(m));
+    }
+}
+
+void test10()
+{
+    int a = 10;
+    auto o = S10(1);
+    auto fun(int m) { return m + a; }
+    assert(o.add!fun(20) == 1+10+20);
+}
+
+/********************************************/
+
+struct S11
+{
+    int m;
+    S11 getVal(alias a)()
+    {
+        return this;
+    }
+    ref getRef(alias a)()
+    {
+        return this;
+    }
+}
+
+void test11()
+{
+    int a;
+    auto s = S11(1);
+
+    if (__ctfe)
+    {
+        // 'this' is currently only
+        // returned by ref in __ctfe
+    }
+    else
+    {
+        ++s.getVal!a().m;
+        assert(s.m == 1);
+    }
+
+    ++s.getRef!a().m;
+    assert(s.m == 2);
+}
+
+/********************************************/
+
 int runTests()
 {
     test1();
@@ -268,6 +451,11 @@ int runTests()
     test4();
     test5();
     test6();
+    test7();
+    test8();
+    test9();
+    test10();
+    test11();
     return 0;
 }
 
